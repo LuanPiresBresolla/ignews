82427336abff3e6b7f638dbf1d943567992aa0cb
Luan Bresolla <luanpires34@gmail.com>
2021-04-09 20:53:29 -0300

    Criando posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,26 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,23 +29,15 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Notas da versão do SDK Platform</strong>
-            <p>Esta página oferece informações de lançamento sobre pacotes do SDK disponíveis para download na guia SDK Platforms do SDK Manager.</p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Notas da versão do SDK Platform</strong>
-            <p>Esta página oferece informações de lançamento sobre pacotes do SDK disponíveis para download na guia SDK Platforms do SDK Manager.</p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Notas da versão do SDK Platform</strong>
-            <p>Esta página oferece informações de lançamento sobre pacotes do SDK disponíveis para download na guia SDK Platforms do SDK Manager.</p>
-          </a>
+          {posts.map(post => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -48,9 +54,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   });
 
-  console.log(response);
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  })
 
   return {
-    props: {}
+    props: {
+      posts,
+    }
   };
 }
